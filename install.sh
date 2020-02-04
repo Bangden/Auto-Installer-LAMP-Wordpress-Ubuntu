95cea9d591950f84d29680b9c2690259e8b8587d
Bang Den <bangden07@gmail.com>
2020-02-05 01:03:03 +0700

    Update install.sh
    Bug Fix

--- install.sh
+++ install.sh
@@ -2,20 +2,26 @@
 echo "\n"
 echo "========================================================="
 echo "=           Pastikan server masih keadaan fresh         ="
-echo "=            Script ini di buat khusus Ubuntu           ="
+echo "=            Script ini di buat khusus UBUNTU           ="
 echo "========================================================="
 echo "                          ||||||                         "
 echo "               Script Copyright by BangDen               "
 echo "========================================================="
-sleep 2
-sudo apt-get install -y install unzip
+sleep 5
+sudo apt-get install -y unzip
+sleep 0.5
 sudo apt-get install -y nano
+sleep 0.5
 sudo apt-get install -y curl
+sleep 0.5
+sudo apt-get install -y git
+sleep 0.5
 sudo apt-get install -y tar
 sleep 1
 openssl enc -d -aes-256-cbc -in main.enc -out eksekusi.sh -pass pass:bangden
+sleep 1
 chmod +x eksekusi.sh
+sleep 1
 sed -i 's/\r//' eksekusi.sh
-echo "\n"
-sh ./eksekusi.sh
 sleep 2
+sh ./eksekusi.sh
